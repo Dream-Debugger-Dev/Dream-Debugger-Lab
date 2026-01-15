74c2efe77fcecb2bdcc15989cfed75817dd9d0ac
Dream Debugger <anshikkewat25@gmail.com>
2026-01-15 21:33:30 +0530

    Create Variables.py to demonstrate variable usage
    Added a Python script demonstrating variable usage, including definitions, assignments, and print statements.

---
+++ 01-Python-Basics/Variables.py
@@ -0,0 +1,33 @@
+#A variable is a name to a memory location in a program
+#Same like in math, find the value of X 🥶, so X value can be anything after solving problem right so X value is veriable depends on Questions, that means value is changeable and changeable value is called variable
+#For variable name we can use a single latter also a word , or more than 1 word but woerd should not be seperated by space instead we can use _ or -
+#Variable is memory location , we use variable for store value with name in momory
+
+#Some Veriables and their values 
+
+name = "Dream Debugger"
+age = 24
+tenth_percentage = 53.45
+
+#We can also store variable in another variable 
+
+newname = name
+
+#Now we can print the value of the variables like this 👇
+
+print(name) #output Dream Debugger
+print(age) #output 24
+print(tenth_percentage) #output 53.45
+
+#we can also print all in once 
+
+print(name, age, tenth_percentage) #output Dream debugger 24 53.45
+
+#We can also print variable value with other sentence
+
+print("My name is", name)
+print("My age is", name)
+
+#if we will print variable with variable is will print 1st variable value
+
+print(newname) #it will print name variable value which is Dream Debugger
