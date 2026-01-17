eaa3bd25bbc9f9b422d7a1d6bf8ceed3af39f776
Dream-Debugger-Dev <anhikkewat25@gmail.com>
2026-01-17 22:51:00 +0530

    Enhance Q1-problem-1.py to include calculations for difference, product, and division of two variables

--- 01-Python-Basics/Q1-problem-1.py
+++ 01-Python-Basics/Q1-problem-1.py
@@ -1,5 +1,11 @@
 #Q1. write a python program to print 2 variables sum
 a = 5
 b = 10  
-sum = a + b
-print("The sum of", a, "and", b, "is:", sum)
+sum = a + b # sum of two variables
+diff = b - a # difference of two variables
+multi = a * b # product of two variables
+divide = b / a # division of two variables
+print("The sum of", a, "and", b, "is:", sum) # print sum
+print("The difference of", a, "and", b, "is:", diff) # print difference
+print("The product of", a, "and", b, "is:", multi) # print product
+print("The division of", a, "and", b, "is:", divide) # print division
