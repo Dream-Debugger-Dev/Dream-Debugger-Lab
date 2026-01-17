db91af36f8fd09f370957deef4b74cfac5793b58
Dream-Debugger-Dev <anhikkewat25@gmail.com>
2026-01-17 22:53:49 +0530

    Add comments-python.py to demonstrate single and multi-line comments in Python

---
+++ 01-Python-Basics/comments-python.py
@@ -0,0 +1,8 @@
+# comments in python
+# single line comment starts with #
+"""
+Multi-line comments in Python are enclosed in triple quotes.
+They can span multiple lines and are ignored by the interpreter.
+"""
+# Example of single line comment
+print("Hello, World!")  # This will print Hello, World! to the console
