0ffb0b944eb25b5f6ec11781205afba425e59006
Dream Debugger <anshikkewat25@gmail.com>
2026-01-20 17:52:18 +0530

    Add script to calculate square area from side length
    This script calculates the area of a square based on user input for the side length.

---
+++ 01-Python-Basics/Q3-input-square-side-print-area.py
@@ -0,0 +1,8 @@
+#Q1. WAP to input side of a square & print its Area 
+
+side = float(input("Enter your square site:")) #user will enter side of a square ,
+
+
+area = side * side # square area in math is side multiple by side  = area (we can also use side **2)
+
+print("Your squar area is", area) #print area after taking input from user of square side   
