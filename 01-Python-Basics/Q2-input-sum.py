fab7b691baa8a0bc5ccb9324d3543b1c0fd431d2
Dream Debugger <anshikkewat25@gmail.com>
2026-01-20 17:41:57 +0530

    Add input and sum functionality in Q2-input-sum.py
    This program takes two numbers as input from the user and prints their sum.

---
+++ 01-Python-Basics/Q2-input-sum.py
@@ -0,0 +1,8 @@
+#Q1. Write a program to input 2 numbers & print their sum
+
+num1 = float(input("Enter 1st number:")) #user will enter 1st number, I have difine input float so if user choose to enter desimal numbers they can
+num2 = float(input("Enter 2nd number:")) #user will enter 2nd number
+
+sum = num1 + num2 #sum veriable performing sum of veriable num1 and num2 values
+
+print("Sum of 1st and 2nd number is:", sum) #after entering 2 numbers is print sum variable value which will be sum of num1 and num2 
