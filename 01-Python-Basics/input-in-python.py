5c8ba6dcd06e88648745af6a4b52fde96e75d3cc
Dream Debugger <anshikkewat25@gmail.com>
2026-01-20 09:46:56 +0530

    Create input-in-python.py for user input example
    Add a Python script to demonstrate input handling.

---
+++ 01-Python-Basics/input-in-python.py
@@ -0,0 +1,17 @@
+#Input in Python 
+#input() statement is used to accept value from the keyboard (using the keyboard) from the user
+
+#input("Enter your name:") #this is how we can use input in python along with text.
+
+name = input("Enter your name:") #we can aslo store the input assigning variable like also we can take multiple inputs from user 👇
+age = input("Enter your age:")
+mark = input("Enter your marks:")
+
+
+
+print("Welcome", name, ) #and then print that variable vaelue what ever user give input along with Welcome message
+print("your age ",age)
+print("your marks", mark)
+# meaning if I run this code first program will ask me for input along with mesage Entery your name:
+# after eentering value from keyboard 
+#it will print Welcome and the value that I entered in first ask
